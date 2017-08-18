b46900c2b9ae2ec64a8ce87b5005e10ac9c21113
Joseph Taylor <jtaylor@pivotal.io>
2017-08-18 16:28:42 +0100

    Package Angular app into Spring Jar

--- src/main/java/com/example/demo/SpringBootNgCliApplication.java
+++ src/main/java/com/example/demo/SpringBootNgCliApplication.java
@@ -2,9 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SpringBootNgCliApplication {
+
+  @Controller
+  static class AppController {
+    @GetMapping("/")
+    public String index(Model model) {
+      return "index";
+    }
+  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootNgCliApplication.class, args);
