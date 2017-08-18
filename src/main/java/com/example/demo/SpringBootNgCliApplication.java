161a84c33b02dd0897d03f5d35d11be5a2b4e16d
Joseph Taylor <jtaylor@pivotal.io>
2017-08-18 16:46:52 +0100

    Use live reload js if ng-serve profile active

--- src/main/java/com/example/demo/SpringBootNgCliApplication.java
+++ src/main/java/com/example/demo/SpringBootNgCliApplication.java
@@ -2,12 +2,35 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.TemplateProcessingParameters;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+import org.thymeleaf.templateresolver.UrlTemplateResolver;
+
+import java.util.Collections;
+import java.util.Set;
 
 @SpringBootApplication
 public class SpringBootNgCliApplication {
+
+  @Bean
+  @Profile("ng-serve")
+  public ITemplateResolver devServerViewResolver() {
+    UrlTemplateResolver resolver = new UrlTemplateResolver() {
+      @Override
+      protected String computeResourceName(TemplateProcessingParameters templateProcessingParameters) {
+        return "http://localhost:4200/";
+      }
+    };
+    Set<String> patterns = Collections.singleton("angular-app-index");
+    resolver.setResolvablePatterns(patterns);
+    resolver.setNonCacheablePatterns(patterns);
+    return resolver;
+  }
 
   @Controller
   static class AppController {
